630a6bd9997c89e9a841e272ce08baada7115cf9
Андрей Сергиенко <dp021294sav@gmail.com>
2018-12-04 10:35:33 +0200

    added project

---
+++ ClosuresTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ClosuresTest
+//
+//  Created by Андрей Сергиенко on 12/4/18.
+//  Copyright © 2018 Андрей Сергиенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var changeColorAction: ((UIColor) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        changeColorAction = {
+            (color: UIColor) in
+            return self.view.backgroundColor = color
+        }
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        changeColorAction!(sender.backgroundColor!)
+    }
+}
